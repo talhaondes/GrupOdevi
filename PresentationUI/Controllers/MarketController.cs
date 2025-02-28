c1b5a17fcc34e99008a472a68727a1df4437495b
beyha <beyha@LENOVO>
2025-02-28 13:07:45 +0300

    Add project files.

---
+++ PresentationUI/Controllers/MarketController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationUI.Controllers
+{
+    public class MarketController : Controller
+    {
+        // GET: AnaSayfa
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ PresentationUI/Controllers/SepetController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationUI.Controllers
+{
+    public class SepetController : Controller
+    {
+        // GET: Sepet
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
